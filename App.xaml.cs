16ff492bd1eab5c076ffc878ceb3dc04d508222c
tebatsomaapola <58773321+tebatsomaapola@users.noreply.github.com>
2025-12-12 19:51:30 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+	public partial class App : Application
+	{
+		
+         static TaskDataBase database;
+
+    public static TaskDataBase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                var path = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                    "tasks.db3"
+                );
+                database = new TaskDataBase(path);
+            }
+            return database;
+        }
+    } 
+        
+    
+	public App()
+	{ 
+		InitializeComponent();
+		MainPage = new NavigationPage(new MainPage())
+    {
+        BarBackgroundColor = Color.DarkBlue,
+        BarTextColor = Color.White
+    };
+		
+
+	}
+
+}}
