19033f934cdb725aa59afb1e3575f8ba853696b1
tebatsomaapola <58773321+tebatsomaapola@users.noreply.github.com>
2025-12-12 19:54:24 +0200

    Add files via upload

---
+++ Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace XamarinForms
+{
+    public class TaskItem 
+    {         
+  
+        public int Id { get; set; }              // Unique identifier
+        public string Title { get; set; }        // Task title
+        public string Description { get; set; }  // Task details
+        public string Status {get; set;}
+        public DateTime DueDate { get; set; }    // Deadline
+        public DateTime CreatedOn {get; set;}
+        public bool IsCompleted { get; set; }    // Status
+    }	
+    
+}
